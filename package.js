daaee14442dac17dc21dd520741ad7f3005fe985
Mike Cunneen <mike@appworkshop.net>
2019-04-23 17:27:17 +0800

    Update package metadata

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'juto:autoform-telephone-input',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Autoform plugin for entering and validating international telephone numbers',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/JutoApp/meteor-autoform-telephone-input.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
